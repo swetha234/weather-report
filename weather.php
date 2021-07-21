732552536dd3bc9b683fb65c43655415a3e32d5a
swetha234 <hgutt001@odu.edu>
2021-07-20 20:30:03 -0400

    skeleton of the page

---
+++ weather.php
@@ -0,0 +1,38 @@
+<html lang="en" class=" -webkit-">
+   <head>
+      <meta charset="UTF-8">
+      <title>Weather Card</title>
+
+   </head>
+   <body>
+      <div class="form">
+         <form style="width:100%;" method="post">
+            <input type="text" class="text" placeholder="Enter city name" name="city" value=""/>
+            <input type="submit" value="Submit" class="submit" name="submit"/>
+         </form>
+      </div>
+    
+      <div class="widget">
+         <div class="Icon">
+           
+         </div>
+         <div class="Info">
+            <div class="temperature">
+              
+            </div>
+            <div class="description">
+               <div class="weather_condition"></div>
+               <div class="place"></div>
+            </div>
+            <div class="description">
+               <div class="weather_condition"></div>
+               <div class="place"></div>
+            </div>
+         </div>
+         <div class="date">
+             
+         </div>
+      </div>
+
+   </body>
+</html>
